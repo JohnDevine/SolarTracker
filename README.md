1dc48e62a9fbc3019befc454f58038ce9e00e3fc
Solar Blue <john.h.devine@gmail.com>
2021-12-17 14:29:11 +0700

    Changed to ArduinoTrace

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # SolarTracker
 Microprocessor used: esp32doit-devkit-v1
 Light Sensor Used: BH1750 
-Servo Used: MG995
+Servo Used: MG995 (only capable of 180 sweep)
+Development environment : VSC and Platformio
 
 This uses a pair of BH1750 light sensors to track the sun and drive 2 x servos to face the brightest sun.
+
+It first does a full sweep then moves to brightest light. After a time it will sweep a small window and move to that position. If the position is at the very end of the window it will initiate a full sweep.
 =======================================================
 BH1750 info.
 I bought the BH1750s here (No idea why AliExpress gives this huge URL):
@@ -13,7 +16,11 @@ Problems & solutions:
 1. I tried quite a few bh1750 libraries and found quite a few could not handle multiple sensors active along with the servo driver. This one worked: hp_BH1750 by Stefan Armborst
 
 2. The sensor gives quite a varied reading. I found that the first reading is usually a real outlier so I use the
-multipleReadLightSensor routine that discards the first reading and does lightSensorMultipleReadCountNeeded reads and after discarding the first reading , averages the others. Also from the datasheet I set the wait time (120ms) after each sense before reading to lightSensorWaitReadTime.
+multipleReadLightSensor routine that discards the first reading and does 
+
+int lightSensorMultipleReadCountNeeded = 5; // Number of readings needed to get a valid reading
+
+reads and after discarding the first reading , averages the others. Also from the datasheet I set the wait time (120ms) after each sense before reading to lightSensorWaitReadTime.
 
 3. The sensor overloads when in bright sun. The sensor can read lux from 1 - 65535 (see datasheet) with an accuracy of 20%. The brightest sunlight is 120,000 lux (wikipedia) so you need a 50% filter above the sensor to get values in range.
 =======================================================
@@ -26,13 +33,18 @@ Problems & solutions:
 1. The PWM can be driven directly off the ESP32. The power though for small sized solar panels (40cm diagonal) is too much draw for the ESP32 and causes intermittent problems particularly on big sweeps. Also note that the speed is dependent on the voltage. I used a voltage regulator and set it to 6V. NOTE this is very important, see next problem.
 
 2. There is no feedback from the servo. This means that if you are at 0 degrees and tell it to go to 180 it will take quite a while to get there and there is no way of knowing if it gets there. (I thought of monitoring the amperage usage on the servo and watching it go to a resting amperage .. maybe later). Given that I use a varying delay based on the previous angle and the new angle required. 
-The Delay is track60DegreeDelayMs needed to force last command thru to the servo.
+The Delay in ms is 
+
+int track60DegreeDelayMs = 300;
+
+needed to force last command thru to the servo.
 For the MG995 the spec sheet says it needs .2 sec to do 60 degrees on 4.8 v Notice for different voltages it can be different (higher voltage less delay) I have chosen .3 secs as I found this from testing
 
 NB I have not allowed for slowing due to loading
 
 3. Different servos have different PWM maximum and minimum pulse widths. You need to look at the spec sheet to get them and also it is worth using a testbed to try them out. I have a program here to do that "TestServo".
 Values for the Aliexpress MG995
+
 #define DEFAULT_uS_LOW 500
 #define DEFAULT_uS_HIGH 2500
 
@@ -49,16 +61,23 @@ and 9 degrees per hour at the equator in summer.
 
 So to track the sun in all seasons to 1 degree accuracy we need to take a reading
 every 5 minutes .. 5*60 = 300 seconds.
+
 #define SUN_TRACK_READING_INTERVAL_SECONDS 300
+
 On a boat this needs to be a much shorter time.
 
 2. When the tracker wakes up each time it checks either side of where it is positioned to find the strongest sun. It uses a window 
-int trackWindowSize = 10;    // Window is this size degrees either side of current position.
+
+int trackWindowSize = 10;    // Window is this size 
+degrees either side of current position.
+
 
 When it is looking in this window it will move in increments of 
+
 int trackFineStepSize = 1;   // degrees
 
 If it hits the end of the window with a maximum reading it will initiate a full scan to find the sun again. It really only does this on a boat that keeps swinging around.
 The full scan  will move in increments of 
-int trackCoarseStepSize = 10; // degrees
 
+int trackCoarseStepSize = 10; // degrees
+=======================================================
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
+#define ARDUINOTRACE_ENABLE 1 // Enable ArduinoTrace (0 = disable, 1 = enable)
+#include <ArduinoTrace.h>
 #include <Arduino.h>
 #include <ESP32Servo.h>
 #include <hp_BH1750.h>
-// Turns on debugging prints if defined
-#define JDDebug false
 
 Servo trackAcrossServo; // Servo for tracking across the sun
 Servo trackUpDownServo; // Servo for tracking up/down the sun
@@ -35,8 +35,8 @@ int trackMaxPulseWidth = 2500;
 
 // Step size for track across servo
 int trackCoarseStepSize = 10; // degrees
-int trackFineStepSize = 1;   // degrees
-int trackWindowSize = 10;    // Window is this size degrees either side of current position
+int trackFineStepSize = 1;    // degrees
+int trackWindowSize = 10;     // Window is this size degrees either side of current position
 
 // BH1750 Sensors
 hp_BH1750 lightSensor1;
@@ -48,13 +48,13 @@ float lightSensor2Reading = 0;
 
 // BH1750 wait read time (in ms) (Data sheet says 120 max)
 int lightSensorWaitReadTime = 120;
+int lightSensorMultipleReadCountNeeded = 5; // Number of readings needed to get a valid reading
 
 // BH1750 Sensor status
 bool lightSensor1Status = false;
 bool lightSensor2Status = false;
 
-bool lightSensorForceRead = true;           // Force a reading from the BH1750 sensor
-int lightSensorMultipleReadCountNeeded = 5; // Number of readings needed to get a valid reading
+bool lightSensorForceRead = true; // Force a reading from the BH1750 sensor
 
 // Create an array of lightSensor readings from 0 to 180 degrees in 1 degree steps
 // The array is used to determine for the position of the servo the light intensity
@@ -78,53 +78,35 @@ bool initLightSensor(hp_BH1750 &sensor, int pin)
 {
     if (sensor.begin(pin) == false)
     {
-        if (JDDebug)
-        {
-            Serial.println("BH1750 Sensor failed to initialize");
-        }
+        TRACE();
 
         return false;
     }
     else
     {
-        if (JDDebug)
-        {
-            Serial.println("BH1750 Sensor initialized");
-        }
+        TRACE();
     }
 
     if (sensor.calibrateTiming() != BH1750_CAL_OK)
     {
-        if (JDDebug)
-        {
-            Serial.println("BH1750 Sensor failed to calibrate timing");
-        }
+        TRACE();
 
         return false;
     }
     else
     {
-        if (JDDebug)
-        {
-            Serial.println("BH1750 Sensor calibrated timing");
-        }
+        TRACE();
     }
 
     if (sensor.start(BH1750_QUALITY_HIGH2, BH1750_MTREG_DEFAULT) != true)
     {
-        if (JDDebug)
-        {
-            Serial.println("BH1750 Sensor failed to start");
-        }
+        TRACE();
 
         return false;
     }
     else
     {
-        if (JDDebug)
-        {
-            Serial.println("BH1750 Sensor started");
-        }
+        TRACE();
     }
 
     return true;
@@ -138,18 +120,12 @@ float singleReadLightSensor(hp_BH1750 &sensor)
 
     while (sensor.hasValue(lightSensorForceRead == false))
     {
-        if (JDDebug)
-        {
-            Serial.println("Waiting for BH1750 Sensor value");
-        }
+        TRACE();
         delay(lightSensorWaitReadTime);
     }
     sensorLuxReading = sensor.getLux();
-    if (JDDebug)
-    {
-        Serial.print("BH1750 Sensor value: ");
-        Serial.println(sensorLuxReading);
-    }
+    TRACE();
+    DUMP(sensorLuxReading);
     return sensorLuxReading;
 }
 // do lightSensorMultipleReadCountNeeded reads of light sensor and average readings
@@ -158,7 +134,7 @@ float multipleReadLightSensor(hp_BH1750 &sensor)
     float lightSensorReading = 0;
     for (int i = 0; i < lightSensorMultipleReadCountNeeded; i++)
     {
-        
+
         // First read is always low an outlier so discard it
         if (i == 0)
         {
@@ -169,7 +145,6 @@ float multipleReadLightSensor(hp_BH1750 &sensor)
         {
             lightSensorReading += singleReadLightSensor(sensor);
         }
-        
     }
     return lightSensorReading / (lightSensorMultipleReadCountNeeded - 1); // average of readings minus the outlier
 }
@@ -178,23 +153,20 @@ float multipleReadLightSensor(hp_BH1750 &sensor)
 float testLightSensor(hp_BH1750 &sensor)
 {
     // do a test read and return value
+
     float testReading = multipleReadLightSensor(sensor);
-    if (JDDebug)
-    {
-        Serial.print("Test reading: ");
-        Serial.println(testReading);
-    }
+    TRACE();
+    DUMP(testReading);
+
     return testReading;
 }
 // Move servo to position
 void moveServo(Servo &servo, int position)
 {
     int servoDelayNeeded = 0;
-    if (JDDebug)
-    {
-        Serial.print("Moving servo to position: ");
-        Serial.println(position);
-    }
+    TRACE();
+    DUMP(position);
+
     // Calculate delay needed to let servo move to position
     int oldPositionDegrees = servo.read();
     if (oldPositionDegrees != position)
@@ -208,21 +180,15 @@ void moveServo(Servo &servo, int position)
             servoDelayNeeded = oldPositionDegrees - position;
         }
         servoDelayNeeded = (servoDelayNeeded * track60DegreeDelayMs) / 60; // Convert to ms
-        if (JDDebug)
-        {
-            Serial.print("Servo delay needed: ");
-            Serial.println(servoDelayNeeded);
-        }
+        TRACE();
+        DUMP(servoDelayNeeded);
 
         servo.write(position);
 
         // Wait for servo to move to position
         delay(servoDelayNeeded); // Delay needed to force last command thru to servo (Deadband)
     }
-    if (JDDebug)
-    {
-        Serial.println("Servo in position");
-    }
+    TRACE();
 }
 bool initServo(Servo &servo, int pin)
 {
@@ -237,19 +203,13 @@ bool initServo(Servo &servo, int pin)
 
     if (servo.attach(pin, trackMinPulseWidth, trackMaxPulseWidth) != 1)
     {
-        if (JDDebug)
-        {
-            Serial.println("Servo failed to initialize");
-        }
+        TRACE();
 
         return false;
     }
     else
     {
-        if (JDDebug)
-        {
-            Serial.println("Servo initialized");
-        }
+        TRACE();
     }
     moveServo(servo, 90); // Set servo to 90 degrees
     return true;
@@ -258,11 +218,7 @@ bool initServo(Servo &servo, int pin)
 // Test servo
 bool testServo(Servo &servo)
 {
-    if (JDDebug)
-    {
-        Serial.println("Testing servo");
-        Serial.println("Moving servo 10 times from 0 to 180 degrees");
-    }
+    TRACE();
     // Loop 10 times and set servo to 0 to 180 degrees
     for (int i = 0; i < 10; i++)
     {
@@ -270,18 +226,12 @@ bool testServo(Servo &servo)
         moveServo(servo, 180); // Set servo to 180 degrees
     }
 
-    if (JDDebug)
-    {
-        Serial.println("Sweep from 180 to 0 degrees");
-    }
+    TRACE();
     for (int i = 180; i >= 0; i -= 10)
     {
         moveServo(servo, i); // move servo to position i
     }
-    if (JDDebug)
-    {
-        Serial.println("Sweep from 0 to 1800 degrees");
-    }
+    TRACE();
     for (int i = 0; i <= 180; i += 10)
     {
         moveServo(servo, i); // move servo to position i
@@ -295,17 +245,12 @@ bool testServo(Servo &servo)
 // This always assumes array is of size 181
 void sweepServoReadLight(float *lightSensorReadings, hp_BH1750 &lightSensor, Servo &servo, int startDegrees, int endDegrees, int stepDegrees)
 {
-    if (JDDebug)
-    {
-        Serial.println("Sweeping servo");
-    }
-// check for valid start and end degrees and if lower than 0 or higher than 180
-#ifdef JDDebug
-    Serial.print("Start: ");
-    Serial.print(startDegrees);
-    Serial.print(" End: ");
-    Serial.println(endDegrees);
-#endif
+    TRACE();
+    DUMP(startDegrees);
+    DUMP(endDegrees);
+
+    // check for valid start and end degrees and if lower than 0 or higher than 180
+
     if (startDegrees < 0)
     {
         startDegrees = 0;
@@ -329,15 +274,11 @@ void sweepServoReadLight(float *lightSensorReadings, hp_BH1750 &lightSensor, Ser
     // Loop through servo positions from startDegrees to endDegrees
     for (int i = startDegrees; i <= endDegrees; i = i + stepDegrees)
     {
-        moveServo(servo, i);                                         // move servo to position i
+        moveServo(servo, i);                                           // move servo to position i
         lightSensorReadings[i] = multipleReadLightSensor(lightSensor); // Read light sensor
-        if (JDDebug)
-        {
-            Serial.print("Light sensor reading at position: ");
-            Serial.print(i);
-            Serial.print(" degrees: ");
-            Serial.println(lightSensorReadings[i]);
-        }
+        TRACE();
+        DUMP(i);
+        DUMP(lightSensorReadings[i]);
     }
 }
 // Return index to highest value in array
@@ -361,10 +302,7 @@ int findMax(float *array, int arraySize)
 // return the best position found or -1 if no position found
 int sweepServoFindBestLightAndPositionThere(float *lightSensorReadings, hp_BH1750 &lightSensor, Servo &servo, int startDegrees, int endDegrees, int stepDegrees)
 {
-    if (JDDebug)
-    {
-        Serial.println("Sweeping across servo looking for strongest reading and position there");
-    }
+    TRACE();
     sweepServoReadLight(lightSensorReadings, lightSensor, servo, startDegrees, endDegrees, stepDegrees);
     int maxIndex = findMax(lightSensorReadings, 181);
     moveServo(servo, maxIndex);
@@ -442,12 +380,12 @@ void loop()
         // look trackWindowSize degrees either side of current posn for new max and move there
         // If the boat moves a lot it will do a full sweep again.
 
-    Serial.println("maxIndex: ");
-    Serial.println(maxIndex);
-    Serial.println("oldMaxIndex: ");
-    Serial.println(oldMaxIndex);
-    Serial.println("trackWindowSize: ");
-    Serial.println(trackWindowSize);
+        Serial.println("maxIndex: ");
+        Serial.println(maxIndex);
+        Serial.println("oldMaxIndex: ");
+        Serial.println(oldMaxIndex);
+        Serial.println("trackWindowSize: ");
+        Serial.println(trackWindowSize);
 
         if (maxIndex == oldMaxIndex - trackWindowSize || maxIndex == oldMaxIndex + trackWindowSize)
         {
